8d2572fc6de09fefb1329bda5040829f192e53de
rlatjdus0814 <kim01093314735@gmail.com>
2021-09-14 15:14:42 +0900

    0914 - filter, sequelize 추가

--- src/route/users.js
+++ src/route/users.js
@@ -5,33 +5,38 @@ faker.locale = "ko";
 
 const userRouter = Router();
 
-let users = [{
-  id: 1,
-  name: "홍길동",
-  age: 21
-}, {
-  id: 2,
-  name: "가나다",
-  age: 21
-}, {
-  id: 3,
-  name: "김철수",
-  age: 21
-}, {
-  id: 4,
-  name: "김영희",
-  age: 21
-}, {
-  id: 5,
-  name: "라바마",
-  age: 21
-}];
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+let users = [];
+for (let i = 1; i < 10000; i += 1) {
+  users.push({
+    id: 1,
+    name: faker.name.lastName() + faker.name.firstName(),
+    age: getRandomInt(15, 50),
+  })
+}
+
+console.log("준비됨")
 
 //전체 조회
 userRouter.get("/", (req, res) => {
+  const { name, age } = req.query;
+  let filteredUsers = users;
+  if (name) {
+    filteredUsers = _.filter(filteredUsers, (user) => {
+      return user.name.includes(name);
+    });
+  }
+  if (age) {
+    filteredUsers = _.filter(filteredUsers, ['age', parseInt(age)]);
+  }
   res.send({
-    count: users.length,
-    users
+    count: filteredUsers.length,
+    filteredUsers
   });
 });
 
